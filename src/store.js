132b377ff39a73e4da442184562f5084e6318dcf
CaitlinCodes <caiit@me.com>
2018-08-28 16:36:53 -0600

    store work

--- src/store.js
+++ src/store.js
@@ -1,16 +1,29 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import Axios from 'axios'
 
 Vue.use(Vuex)
 
+let battleApi = Axios.create({
+  baseURL: 'https://inspire-server.herokuapp.com/cards',
+  timeout: 3000
+})
+
 export default new Vuex.Store({
   state: {
-
+    games: []
   },
   mutations: {
-
+    setGames(state, data) {
+      state.games = data
+    }
   },
   actions: {
-
+    //   search({ commit, dispatch ), newGame) {
+    //     battleApi.get(newGame)
+    //     .then(res => {
+    //       commit('setGame', res.data.results)
+    //     })
+    //   }
   }
 })
