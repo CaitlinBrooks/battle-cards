e30bce6219a1566625a7d63d9b43a1a7cdbc6c1d
CaitlinCodes <caiit@me.com>
2018-08-28 15:51:31 -0600

    framework and what to keep

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,20 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
+    <PlayerHand />
+    <EnemyHand />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+  // @ is an alias to /src
+  import PlayerHand from '@/components/PlayerHand.vue'
+  import EnemyHand from '@/components/EnemyHand.vue'
 
-export default {
-  name: 'home',
-  components: {
-    HelloWorld
+  export default {
+    name: 'home',
+    components: {
+      PlayerHand
+    }
   }
-}
 </script>
