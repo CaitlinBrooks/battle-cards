406018b04ead9449db75da85285a69ccf405ad94
CaitlinCodes <caiit@me.com>
2018-09-02 16:47:01 -0600

    cards

--- src/store.js
+++ src/store.js
@@ -18,36 +18,41 @@ export default new Vuex.Store({
     enemyCard: {},
     game: {}
   },
+
+
   mutations: {
     setGame(state, data) {
       state.game = data
+    },
+    setPlayerCard(state, playerCard) {
+      state.playerCard = playerCard
+    },
+    setEnemyCard(state, enemyCard) {
+      state.enemyCard = enemyCard
+    },
+
+
+    actions: {
+      newGame({ commit }) {
+        battleApi.post('').then(res => {
+          console.log('it works', res)
+          commit('setGame', res.data)
+        })
+      },
+      attack({ commit, dispatch, state }, attackInfo) {
+        battleApi.put('/' + state.game.id, attackInfo).then(res => {
+          console.log('attack came back', res)
+          dispatch('getGame', state.game.id)
+        })
+      },
+      getGame({ commit, state }) {
+        battleApi.get('/' + state.game.id).then(res => {
+          console.log('game came back', res)
+          commit('getGame', state.game.id)
+        })
+      },
+      setPlayerCard({ commit, dispatch }, playerCard) {
+        commit('setPlayerCard', playerCard)
+      }
     }
-  },
-  // setPlayerCard(state, playerCard) {
-  //   state.PlayerCard = playerCard
-  // },
-  // setEnemyCard(state, enemyCard) {
-  //   state.EnemyCard = enemyCard
-  // },
-  // setPlayerCard(state, player)
-  actions: {
-    newGame({ commit }) {
-      battleApi.post('').then(res => {
-        console.log('it works', res)
-        commit('setGame', res.data)
-      })
-    },
-    attack({ commit, dispatch, state }, attackInfo) {
-      battleApi.put('/' + state.game.id, attackInfo).then(res => {
-        console.log('attack came back', res)
-        dispatch('getGame', state.game.id)
-      })
-    },
-    getGame({ commit, state }) {
-      battleApi.get('/' + state.game.id).then(res => {
-        console.log('game came back', res)
-        commit('getGame', state.game.id)
-      })
-    },
-  }
-})
+  })
