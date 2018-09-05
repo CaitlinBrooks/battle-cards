d6661589ef415d72281876a76a6ed8f3aabc86fc
CaitlinCodes <caiit@me.com>
2018-09-04 20:03:15 -0600

    template for component

--- src/components/GameFinished.vue
+++ src/components/GameFinished.vue
@@ -3,5 +3,10 @@
     <div v-if="game.winner.id == game.players[0].id">
       <h2>Winner!</h2>
     </div>
-    <div v-else="game.winner.id == game.players[1]" </div>
+    <div v-else="game.winner.id">
+      <h2>You Lose!</h2>
+    </div>
+    <div v-else>
+      <h2>Battle Cats Tied!</h2>
+    </div>
 </template>
